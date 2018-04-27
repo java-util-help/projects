01d3a7d49dd78ecc4cff8986ab79cb573c1e46f1
boratanrikulu <boratanrikulu@gmail.com>
2018-04-27 15:32:42 +0300

    upload the uncompleted project

---
+++ PT2-Bookstore/boratanrikulu/Mains/BookStore.java
@@ -0,0 +1,157 @@
+/*
+	NOTE: This program is written without using any IDE.
+	Tests were completed manually by using java-9-openjdk.
+	Please read the README.md file that is in the project.
+*/
+package bookstore;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BookStore {
+
+	private final String name;
+	private ArrayList<Section> sections;
+	private static Employee employeeTemp;
+	private final Scanner scan;
+	// constructor
+	public BookStore(String name) {
+		if(name.equals(""))
+			throw new IllegalArgumentException("(!) The Name of The Book Store is not entered.");
+
+		this.name = name;
+
+		scan = new Scanner(System.in);
+		// Creates Default Section Objects
+		sections = new ArrayList<Section>();
+
+		createHistorySection(); // sections[0]
+		createMythologySection(); // sections[1]
+		createPoemSection(); // sections[2]
+		createSciFiSection(); // sectons[3]
+	}
+
+	// creates default section objects
+	private void createHistorySection() {
+		// creates a history class with an employee
+		this.employeeTemp = new Staff("Aslı Türk", Employee.Gender.F, 30, 7);
+		Section history = new History(this.employeeTemp);
+
+		this.sections.add(history);
+	}
+	private void createMythologySection() {
+		// creates a mythology class with an employee
+		this.employeeTemp = new Staff("Kuzey Batı", Employee.Gender.M, 30, 7);
+		Section mythology = new Mythology(this.employeeTemp);
+
+		this.sections.add(mythology);
+	}
+	private void createPoemSection() {
+		// creates a poem class with an employee
+		this.employeeTemp = new Staff("Asya Asya", Employee.Gender.F, 30, 7);
+		Section poem = new Poem(this.employeeTemp);
+
+		this.sections.add(poem);
+	}
+	private void createSciFiSection() {
+		// creates a sciFi class with an employee
+		this.employeeTemp = new Staff("Bora Tanrıkulu", Employee.Gender.M, 30, 7);
+		Section sciFi = new SciFi(this.employeeTemp);
+
+		this.sections.add(sciFi);
+	}
+
+	// methods
+	public void showAllBooks() {
+		System.out.println("##########################################################################");
+		for(Section temp : this.sections) {
+			System.out.println("#");
+			temp.showTheBooks();
+		}
+		System.out.println("#");
+		System.out.println("##########################################################################");
+
+		System.out.print("\n(->) Push enter to return Main Menu.");
+		scan.nextLine();
+	}
+
+	public void addABook() {
+		boolean flag = true;
+
+		while(flag) {
+			clear();
+			int counter = 0;
+			System.out.println("##########################################################################");
+			System.out.println("# Which Section to Add A Book ?                                          #");
+			System.out.println("#");
+			for(Section temp : this.sections) {
+				System.out.println("# "+(++counter)+") " + temp.getName());
+			}
+			System.out.println("#");
+			System.out.println("# 9) Return to Upper Menu                                                #");
+			System.out.println("##########################################################################");
+			System.out.print("\t Menu Option: "); String menuOption = scan.nextLine();
+
+			switch(menuOption) {
+				case "1":
+					takeInfoOfBook();
+
+					clear();
+					break;
+				case "2":
+					takeInfoOfBook();
+
+					clear();
+					break;
+				case "3":
+					takeInfoOfBook();
+
+					clear();
+					break;
+				case "4":
+					takeInfoOfBook();
+
+					clear();
+					break;
+				case "9":
+					flag = false;
+					break;
+				default:
+					System.out.print("\n(!) Select appropriate menu options.");
+					scan.nextLine();
+					break;
+			}
+		}
+		
+	}
+
+	public void sellABook() {
+		String title;
+		String author;
+		String publishDate;
+		int price;
+
+		System.out.println();
+		System.out.println("# Title of The Book: "); title = scan.nextLine();
+		System.out.println("# Author of The Book: "); author = scan.nextLine();
+		System.out.println("# Publish Date of The Book: "); publishDate = scan.nextLine();
+		System.out.println("# Price of The Book: "); price = scan.nextInt();
+	}
+
+	// sub-methods
+	private void takeInfoOfBook() {
+		
+	}
+
+	// clear
+	private void clear() {
+		System.out.print("\033[H\033[2J");  
+   		System.out.flush();  
+	}
+
+	// getters
+	public String getName() {
+		return this.name;
+	}
+
+	
+}
