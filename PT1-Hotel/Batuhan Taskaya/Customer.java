e8dfa93f4f2d8ee79889be8b7690619576fbb657
ayyucedemirbas <ayyucedemirbas@windowslive.com>
2018-08-14 18:02:37 +0300

    Batuhan Taskaya

---
+++ PT1-Hotel/Batuhan Taskaya/Customer.java
@@ -0,0 +1,79 @@
+
+/**
+ *
+ * @author Ayyuce
+ */
+public class Customer {
+
+    private String Name = "";
+    private String Surname = "";
+    private String Occupation = "";
+    private int Age = 18; //Default age is 18
+
+    public enum Sex {
+        Female,
+        Male,
+        Other
+    };
+    Sex sex;
+
+    public Customer() {
+
+    }
+
+    public Customer(String Name, String Surname, String Occupation, int Age, Sex sex) {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Occupation = Occupation;
+        this.Age = Age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String Surname) {
+        this.Surname = Surname;
+    }
+
+    public String getOccupation() {
+        return Occupation;
+    }
+
+    public void setOccupation(String Occupation) {
+        this.Occupation = Occupation;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public void Display() {
+        if (this.Age != 0) {
+            System.out.println("Customer -> Name: " + Name + " Surname: " + Surname + " Occupation: " + Occupation + " Age: " + Age + " Sex: " + sex);
+
+
+    }
+}
+}
---
+++ PT1-Hotel/Batuhan Taskaya/Room.java
@@ -0,0 +1,100 @@
+
+/**
+ *
+ * @author Ayyuce
+ */
+public class Room {
+
+    private String RoomNumber;
+    private double Bill;
+    private final int CustomerCount;
+
+   
+
+    public enum Status {
+        Avalible,
+        NotAvalible
+    }
+    Status S;
+
+    public enum Types {
+        KingSuite,
+        BedRoomFor2,
+        BedRoomFor3,
+        BedRoomFor4,
+        BedRoomFor5,
+        EconomicRoom
+
+    };
+    Types type;
+    Customer[] customers;
+
+    public Room(String RoomNumber, double Bill, int CustomerCount, Types type) {
+        this.RoomNumber = RoomNumber;
+        this.Bill = Bill;
+        this.CustomerCount = CustomerCount;
+        this.type = type;
+        customers = new Customer[CustomerCount];
+        for (int i = 0; i < CustomerCount; i++) {
+            customers[i] = new Customer();
+        }
+        this.S = Status.Avalible;
+    }
+
+     public int getCustomerCount() {
+        return CustomerCount;
+    }
+     
+    public Status getS() {
+        return S;
+    }
+
+    public void setS(Status S) {
+        this.S = S;
+    }
+
+    public Customer[] getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Customer[] customers) {
+        this.customers = customers;
+    }
+
+    public String getRoomNumber() {
+        return RoomNumber;
+    }
+
+    public void setRoomNumber(String RoomNumber) {
+        this.RoomNumber = RoomNumber;
+    }
+
+    public double getBill() {
+        return Bill;
+    }
+
+    public void setBill(double Bill) {
+        this.Bill = Bill;
+    }
+
+    public Types getType() {
+        return type;
+    }
+
+    public void setType(Types type) {
+        this.type = type;
+    }
+
+    public void Display() {
+        System.out.println("Room " + RoomNumber + " Room Type: " + type + " Status: " + S + " Bill: " + Bill);
+
+    }
+
+    public void DisplayCutomers() {
+        for (Customer c1 : customers) {
+           c1.Display();
+            
+        }
+
+    }
+}
